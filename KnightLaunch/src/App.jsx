924d7ca1cbcf22951a47c3fa0307e2ef672cf983
Siddharth <siddharthpg18@gmail.com>
2024-12-01 11:04:21 +0530

    create token functionality added

--- KnightLaunch/src/App.jsx
+++ KnightLaunch/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
+import React from 'react';
 import { TokenLaunchpad } from './components/Tokenlaunchpad';
-
-// wallet adapter imports
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import {
     WalletModalProvider,
